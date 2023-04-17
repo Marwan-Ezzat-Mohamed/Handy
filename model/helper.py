993de73167d6437dbe363de113b6612ecc4225cc
marwan-ezzat-mohamed <marwan.ezzat.badawi@gmail.com>
2023-04-17 16:46:30 +0200

    rewrite split data function

--- model/helper.py
+++ model/helper.py
@@ -3,48 +3,113 @@ import shutil
 from tqdm import tqdm
 import numpy as np
 from mediapipeHelper import *
-import math
-import json
-import time
+import random
 
 
-def make_test_val_data(limit=10,
-                       min_videos_per_action=50):
+import random
+
+
+def split_arr(dataset: set,
+              train_ratio: float,
+              val_ratio: float) -> tuple:
+    """
+    Split a given dataset into three subsets: train, validation, and test.
+
+    Args:
+    dataset (set): The dataset to be split.
+    train_ratio (float): The ratio of data to use for the training set.
+    val_ratio (float): The ratio of data to use for the validation set.
+
+    Returns:
+    A tuple containing three sets: train set, validation set, and test set.
+    """
+    if len(dataset) < 3:
+        raise ValueError('The dataset must have at least 3 elements.')
+
+    # Calculate the size of each subset based on the given ratios
+    data_size = len(dataset)
+    train_size = int(train_ratio * data_size)
+    val_size = int(val_ratio * data_size)
+    test_size = data_size - train_size - val_size
+
+    # Randomly shuffle the dataset
+    shuffled_data = random.sample(dataset, data_size)
+
+    # check if the val and test sets are empty
+    if val_size == 0:
+        val_size = 1
+    if test_size == 0:
+        test_size = 1
+
+    train_size = data_size - val_size - test_size
+
+    # Split the shuffled dataset into train, validation, and test sets
+    train_set = set(shuffled_data[:train_size])
+    val_set = set(shuffled_data[train_size:train_size+val_size])
+    test_set = set(shuffled_data[train_size+val_size:])
+
+    return train_set, val_set, test_set
+
+
+def split_data(limit=50,
+               min_videos_per_action=5):
+    MAIN_DATA_FOLDER = 'MP_DATA_NEW'
     # count the number of videos that are greater than min_videos_per_action
     number_of_videos = [f for f in os.listdir(
-        'MP_Data') if len(os.listdir(os.path.join('MP_Data', f))) >= min_videos_per_action]
+        MAIN_DATA_FOLDER) if len(os.listdir(os.path.join(MAIN_DATA_FOLDER, f))) >= min_videos_per_action]
     pbar = tqdm(total=min(len(number_of_videos), limit),
                 desc='Generating test and val data')
-    words_folder = os.listdir('MP_Data')
+    words_folder = os.listdir(MAIN_DATA_FOLDER)
     # sort the words folder by the folder that has the most videos
     words_folder.sort(key=lambda x: len(os.listdir(
-        os.path.join('MP_Data', x))), reverse=True)
+        os.path.join(MAIN_DATA_FOLDER, x))), reverse=True)
     for word_folder in words_folder:
         if limit == 0:
             break
-        action_path = os.path.join('MP_Data', word_folder)
+        action_path = os.path.join(MAIN_DATA_FOLDER, word_folder)
         number_of_videos = len(os.listdir(action_path))
         if number_of_videos < min_videos_per_action:
             continue
         limit -= 1
-        number_of_videos_for_test = max(int(number_of_videos * 0.1), 2)
-        number_of_videos_for_train = number_of_videos - number_of_videos_for_test
 
-        # val data is used for generating the new data (will be used for training)
-        # copy videos to train folder
-        for video in os.listdir(action_path)[:number_of_videos_for_train]:
-            video_path = os.path.join(action_path, video)
-            new_video_path = os.path.join('MP_Val', word_folder)
-            os.makedirs(new_video_path, exist_ok=True)
-            shutil.copy(video_path, new_video_path)
+        val_percent = 0.1
+        train_percent = 0.80
 
-        # copy videos to test folder
-        for video in os.listdir(action_path)[number_of_videos_for_train:]:
-            video_path = os.path.join(action_path, video)
-            new_video_path = os.path.join('MP_Test', word_folder)
-            os.makedirs(new_video_path, exist_ok=True)
-            shutil.copy(video_path, new_video_path)
+        # remove the  _rotated_ and  _resized_x.xx  from the video file names
+        # insert the new video file names in a set
+        filenames = set()
+        for video_folder in os.listdir(action_path):
+            filename = video_folder.split('_moving')[0]
+            filenames.add(filename)
 
+        # split the set into two sets
+        train_filenames, val_filenames, test_filenames = split_arr(
+            filenames, train_percent, val_percent)
+
+        # create the test folder
+        test_folder = os.path.join('MP_Test', word_folder)
+        if not os.path.exists(test_folder):
+            os.makedirs(test_folder)
+        # create the val folder
+        val_folder = os.path.join('MP_Val', word_folder)
+        if not os.path.exists(val_folder):
+            os.makedirs(val_folder)
+        # create the train folder
+        train_folder = os.path.join('MP_Train', word_folder)
+        if not os.path.exists(train_folder):
+            os.makedirs(train_folder)
+
+        for video_folder in os.listdir(action_path):
+            filename = video_folder.split('_moving')[0]
+            if filename in test_filenames:
+                shutil.copy(os.path.join(action_path, video_folder),
+                            os.path.join(test_folder, video_folder))
+            elif filename in val_filenames:
+                shutil.copy(os.path.join(action_path, video_folder),
+                            os.path.join(val_folder, video_folder))
+            elif filename in train_filenames:
+                shutil.copy(os.path.join(action_path, video_folder),
+                            os.path.join(train_folder, video_folder))
         pbar.update(1)
 
 
@@ -59,7 +124,7 @@ def get_all_vids_paths(folder_name):
 
 
 if __name__ == '__main__':
-    make_test_val_data()
+    split_data()
     # videos = get_all_vids_paths('MSASL')
     # # calculate time taken to process all the videos
     # start = time.time()
